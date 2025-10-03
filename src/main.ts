009742aaed85d974ff70cb3f424a1a08ebe8555f
YoussefAhmed100 <youssefahmed16110@gmail.com>
2025-10-03 22:22:10 +0300

    update railway ports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ValidationPipe, Logger, ClassSerializerInterceptor } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -9,35 +8,40 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const logger = new Logger('Bootstrap');
-app.enableCors({
-  origin: '*',
-  methods: ['GET'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
-  // Security middleware
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.use(helmet());
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    
-  }));
-  // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Teacherz Streaming API')
     .setDescription('API for TeacherZ ')
     .setVersion('1.0')
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'Authorization')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'Authorization',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.APP_PORT || 3000;
-  await app.listen(port,'0.0.0.0');
-  logger.log(`Server running on http://localhost:${port}`);
-  logger.log(`Swagger: http://localhost:${port}/api/docs`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  logger.log(`🚀 Server running on http://localhost:${port}`);
+  logger.log(`📘 Swagger: http://localhost:${port}/api/docs`);
 }
 bootstrap();
