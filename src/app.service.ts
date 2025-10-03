314242c4ecde6d37c4706babaab9baad0e1591d7
YoussefAhmed100 <youssefahmed16110@gmail.com>
2025-10-03 22:03:35 +0300

    update welcome massage

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from movie app!';
+    return 'Hello from Teacherz app!';
   }
 }
